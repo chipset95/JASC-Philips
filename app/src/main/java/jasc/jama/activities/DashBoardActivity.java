e98d9c4507fae9052359ed3fc489b2f76f07765a
chipset <chipset95@yahoo.co.in>
2015-10-17 15:49:46 +0530

    Drawer

--- app/src/main/java/jasc/jama/activities/DashBoardActivity.java
+++ app/src/main/java/jasc/jama/activities/DashBoardActivity.java
@@ -3,12 +3,20 @@ package jasc.jama.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import jasc.jama.R;
 
 public class DashBoardActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     public static Intent createIntent(Context context) {
         return new Intent(context, DashBoardActivity.class);
@@ -18,6 +26,8 @@ public class DashBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.dash_toolbar);
+        setSupportActionBar(toolbar);
 
         new AlertDialog.Builder(DashBoardActivity.this)
                 .setTitle("DISCLAIMER")
@@ -25,5 +35,29 @@ public class DashBoardActivity extends AppCompatActivity {
                 .setMessage("ABCD")
                 .setPositiveButton(android.R.string.ok, null)
                 .create().show();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(DashBoardActivity.this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item) || onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawers();
+        }
     }
 }
